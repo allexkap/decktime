1ae391682aa89d62d0295324176ea867a5239093
allexkap <aekpstn@gmail.com>
2025-01-23 17:51:02 +0300

    add events

--- src/db.rs
+++ src/db.rs
@@ -1,16 +1,28 @@
-use log::{debug, error, info};
+use log::{debug, error, info, warn};
 use rusqlite::{Connection, Error, Result};
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     time::{SystemTime, UNIX_EPOCH},
 };
 
 pub type AppId = u32;
+pub const THIS_APP_ID: AppId = 0;
+pub const STEAM_APP_ID: AppId = 1;
+
+#[derive(Debug, Clone, Copy)]
+pub enum EventType {
+    Running = 0,
+    Started,
+    Stopped,
+    Suspended,
+    Resumed,
+}
 
 pub struct DeckDB {
     conn: Connection,
     cache: Option<HashMap<AppId, u64>>,
-    cache_timestamp: u64,
+    cache_timestamp_h: u64,
+    running_apps: HashSet<AppId>,
 }
 
 impl DeckDB {
@@ -19,76 +31,95 @@ impl DeckDB {
 
         let tx = conn.transaction()?;
         tx.execute(
-            "create table if not exists objects (
-                object_id integer not null,
-                app_id integer unique not null,
-                alias text,
+            "create table if not exists objects ( \
+                object_id integer not null, \
+                app_id integer unique not null, \
+                alias text, \
                 primary key (object_id)
             );",
             (),
         )?;
         tx.execute(
-            "create table if not exists timeline (
-                timestamp integer not null,
-                object_id integer not null,
-                value integer not null,
-                primary key (timestamp, object_id),
+            "create table if not exists timeline ( \
+                timestamp integer not null, \
+                object_id integer not null, \
+                value integer not null, \
+                primary key (timestamp, object_id), \
                 foreign key (object_id) references objects (object_id)
             )",
             (),
         )?;
+
+        tx.execute(
+            "create table if not exists events ( \
+                timestamp integer not null, \
+                object_id integer not null, \
+                event_type integer not null, \
+                foreign key (object_id) references objects (object_id)
+            )",
+            (),
+        )?;
+        assert_eq!(EventType::Running as u32, 0);
+        tx.execute(
+            "update events set event_type = ?1 where event_type = ?2",
+            (EventType::Stopped as u32, EventType::Running as u32),
+        )?;
+
         tx.commit()?;
 
         Ok(DeckDB {
             conn,
             cache: None,
-            cache_timestamp: 0,
+            cache_timestamp_h: 0,
+            running_apps: HashSet::new(),
         })
     }
 
-    fn load_cache(&mut self, timestamp: u64) {
+    fn get_object_id(conn: &Connection, app_id: AppId) -> Result<u32> {
+        conn.query_row(
+            "select object_id from objects where app_id = ?1",
+            (app_id,),
+            |row| row.get(0),
+        )
+        .or_else(|err| match err {
+            Error::QueryReturnedNoRows => conn.query_row(
+                "insert into objects (app_id) values (?1) returning object_id",
+                (app_id,),
+                |row| row.get(0),
+            ),
+            err => Err(err),
+        })
+    }
+
+    fn load_cache(&mut self, timestamp_h: u64) {
         let mut stmt = self
             .conn
             .prepare_cached(
-                "select app_id, value from timeline
-                join objects on timeline.object_id = objects.object_id
+                "select app_id, value from timeline \
+                join objects on timeline.object_id = objects.object_id \
                 where timestamp = ?1",
             )
-            .expect("sql syntax error");
+            .expect("sql call failed");
 
         let cache = stmt
-            .query_map((timestamp,), |row| Ok((row.get(0)?, row.get(1)?)))
-            .expect("database error that should not happen")
+            .query_map((timestamp_h,), |row| Ok((row.get(0)?, row.get(1)?)))
+            .expect("sql binding failed")
             .filter_map(Result::ok)
             .collect();
 
         self.cache = Some(cache);
-        self.cache_timestamp = timestamp;
+        self.cache_timestamp_h = timestamp_h;
     }
 
     fn save_cache(&mut self) -> Result<()> {
         let tx = self.conn.transaction()?;
         if let Some(cache) = self.cache.as_ref() {
+            let mut stmt = tx
+                .prepare_cached("insert or replace into timeline values (?1, ?2, ?3)")
+                .expect("sql call failed");
             for (&app_id, &value) in cache.iter() {
-                let object_id: u32 = match tx.query_row(
-                    "select object_id from objects where app_id = ?1",
-                    (app_id,),
-                    |row| row.get(0),
-                ) {
-                    Ok(entry) => entry,
-                    Err(Error::QueryReturnedNoRows) => tx.query_row(
-                        "insert into objects (app_id) values (?1) returning object_id",
-                        (app_id,),
-                        |row| row.get(0),
-                    )?,
-                    Err(err) => {
-                        panic!("{err}");
-                    }
-                };
-                tx.execute(
-                    "insert or replace into timeline values (?1, ?2, ?3)",
-                    (self.cache_timestamp, object_id, value),
-                )?;
+                let object_id = Self::get_object_id(&(*tx), app_id)?;
+                stmt.execute((self.cache_timestamp_h, object_id, value))?;
             }
         }
         tx.commit()
@@ -107,17 +138,80 @@ impl DeckDB {
     }
 
     pub fn commit(&mut self, timestamp: SystemTime) -> Result<()> {
-        let timestamp = timestamp.duration_since(UNIX_EPOCH).unwrap().as_secs() / 60 / 60;
-        debug!("commit with timestamp={timestamp}");
+        let timestamp_h = timestamp.duration_since(UNIX_EPOCH).unwrap().as_secs() / 60 / 60;
+        debug!("commit with timestamp={timestamp_h}");
+
+        self.event(timestamp, THIS_APP_ID, EventType::Running)?;
 
         self.save_cache()?;
-
-        if self.cache.is_none() || timestamp != self.cache_timestamp {
+        if self.cache.is_none() || timestamp_h != self.cache_timestamp_h {
             info!("reload cache");
-            self.load_cache(timestamp);
+            self.load_cache(timestamp_h);
         }
 
         Ok(())
+    }
+
+    pub fn event(
+        &mut self,
+        timestamp: SystemTime,
+        app_id: AppId,
+        event_type: EventType,
+    ) -> Result<()> {
+        let timestamp_s = timestamp.duration_since(UNIX_EPOCH).unwrap().as_secs();
+
+        if app_id != THIS_APP_ID {
+            info!("new event: app_id={app_id}; event_type={event_type:?}");
+        }
+
+        const SQL_INSERT: &'static str =
+            "insert into events (timestamp, object_id, event_type) values (?1, ?2, ?3)";
+        match event_type {
+            EventType::Started | EventType::Stopped => {
+                let object_id = Self::get_object_id(&self.conn, app_id)?;
+                self.conn
+                    .execute(SQL_INSERT, (timestamp_s, object_id, event_type as u32))?;
+            }
+
+            EventType::Suspended | EventType::Resumed | EventType::Running => {
+                let tx = self.conn.transaction()?;
+                if let EventType::Running = event_type {
+                    assert_eq!(EventType::Running as u32, 0);
+                    tx.execute(
+                        "delete from events where event_type = ?1",
+                        (EventType::Running as u32,),
+                    )?;
+                }
+                {
+                    let mut stmt = tx.prepare_cached(SQL_INSERT)?;
+                    for &app in self.running_apps.iter() {
+                        stmt.execute((
+                            timestamp_s,
+                            DeckDB::get_object_id(&(*tx), app)?,
+                            event_type as u32,
+                        ))?;
+                    }
+                }
+                tx.commit()?;
+            }
+        }
+
+        let ok = match event_type {
+            EventType::Started => self.running_apps.insert(app_id),
+            EventType::Stopped => self.running_apps.remove(&app_id),
+            _ => true,
+        };
+
+        if !ok {
+            error!("duplicated event: app_id={app_id}; event_type={event_type:?}");
+            debug_assert!(false, "duplicated event");
+        }
+
+        Ok(())
+    }
+
+    pub fn get_running_apps(&self) -> HashSet<AppId> {
+        self.running_apps.clone()
     }
 }
 
--- src/observer.rs
+++ src/observer.rs
@@ -1,6 +1,5 @@
 use crate::db;
-use itertools::Itertools;
-use log::{info, warn};
+use log::{debug, error, info, warn};
 use std::{cell::RefCell, fs, rc::Rc, time::SystemTime};
 
 fn find_pid_by_name(appname: &str) -> Option<u32> {
@@ -15,44 +14,61 @@ fn find_pid_by_name(appname: &str) -> Option<u32> {
         .next()
 }
 
+fn get_app_id_by_pid(pid: u32) -> Option<u32> {
+    let cmdline = fs::read_to_string(format!("/proc/{pid}/cmdline")).ok()?;
+    let pos = cmdline.find("AppId=")? + 6;
+    let len = cmdline[pos..].find("\x00")?;
+    cmdline[pos..pos + len].parse::<u32>().ok()
+}
+
 pub fn get_update_func(value: u64, ref_db: Rc<RefCell<db::DeckDB>>) -> impl FnMut(SystemTime) {
     let mut ppid = None;
-    return move |_| {
+
+    return move |now| {
+        let mut db = ref_db.borrow_mut();
+
         if ppid.is_none() {
             ppid = find_pid_by_name("steam");
-            let Some(ppid) = ppid else {
+            if ppid.is_none() {
                 return;
             };
-            info!("steam pid = {ppid}");
+            info!("steam pid={}", ppid.unwrap());
+            db.event(now, db::STEAM_APP_ID, db::EventType::Started)
+                .expect("event error");
         }
 
-        let Ok(dir_entry) = fs::read_dir(format!("/proc/{}/task", ppid.unwrap())) else {
-            info!("steam pid not found");
+        let Ok(dir) = fs::read_dir(format!("/proc/{}/task", ppid.unwrap())) else {
+            warn!("steam pid not found");
+            db.event(now, db::STEAM_APP_ID, db::EventType::Stopped)
+                .expect("event error");
             ppid = None;
             return;
         };
 
-        dir_entry
-            .filter_map(Result::ok)
-            .filter_map(|entry| fs::read_to_string(entry.path().join("children")).ok())
+        let mut closed_apps = db.get_running_apps();
+
+        dir.filter_map(|entry| fs::read_to_string(entry.ok()?.path().join("children")).ok())
             .flat_map(|pids| {
                 pids.split_ascii_whitespace()
-                    .filter_map(|pid| pid.parse::<u32>().ok())
+                    .filter_map(|pid| pid.parse().ok())
                     .collect::<Vec<u32>>()
             })
-            .filter_map(|pid| {
-                let cmdline = fs::read_to_string(format!("/proc/{pid}/cmdline")).ok()?;
-                let pos = cmdline.find("AppId=")? + 6;
-                let len = cmdline[pos..].find("\x00")?;
-                match cmdline[pos..pos + len].parse::<u32>() {
-                    Ok(val) => Some(val),
-                    Err(err) => {
-                        warn!("parse app_id error: {err}; cmdline={cmdline:?}");
-                        None
+            .for_each(|pid| {
+                if let Some(app_id) = get_app_id_by_pid(pid) {
+                    if closed_apps.remove(&app_id) == false {
+                        db.event(now, app_id, db::EventType::Started)
+                            .expect("event error");
                     }
+                    db.update(app_id, value);
                 }
-            })
-            .unique()
-            .for_each(|app_id| ref_db.borrow_mut().update(app_id, value));
+            });
+
+        closed_apps.iter().for_each(|&app_id| {
+            // idk how else to skip special values...
+            if app_id > 10 {
+                db.event(now, app_id, db::EventType::Stopped)
+                    .expect("event error");
+            }
+        });
     };
 }
